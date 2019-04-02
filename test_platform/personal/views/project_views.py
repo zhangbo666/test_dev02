6becbf013cb8a533e9905e163f235d4bf15c4962
zhangbo666 <78038490@qq.com>
2019-04-03 03:24:41 +0800

    2019-04-03 03:24 提交

--- test_platform/personal/views/project_views.py
+++ test_platform/personal/views/project_views.py
@@ -1,5 +1,6 @@
 __author__ = 'zhangbo'
 
+from django.http import HttpResponse,HttpResponseRedirect
 
 from django.shortcuts import render
 
@@ -7,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 
 from personal.models.project import Project
 
-import datetime
+from datetime import datetime
 
 # 登录成功，默认项目管理页
 @login_required
@@ -34,13 +35,47 @@ def add_project(request):
 
         status = request.POST.get("status","")
 
-        print (project_name)
-        print (project_describe)
-        print (status)
+        if project_name == "":
 
-        Project.objects.create(name=project_name,describe=project_describe,status=status,
-                               create_time=datetime(2019,04,02,19,50,00))
+            return render(request,"project.html",{"type":"add","name_error":"项目名称不能为空"})
+
+        else:
+
+            Project.objects.create(name=project_name,describe=project_describe,status=status,
+                                   create_time=datetime(2019,4,3,00,10,00))
+
+            return HttpResponseRedirect("/project/")
 
 
-        # return render(request,"project.html",{"type":"add"})
+# 编辑项目
+@login_required
+def edit_project(request,pid):
 
+    if request.method == 'GET':
+
+        pro = Project.objects.get(id=pid)
+        print (pro.status)
+        print (pro.name)
+        print (pro.describe)
+        print (pro.create_time)
+
+        return render(request,"project.html",{"type":"edit"})
+
+    # elif request.method == 'POST':
+    #
+    #     project_name = request.POST.get("project_name","")
+    #
+    #     project_describe = request.POST.get("project_describe","")
+    #
+    #     status = request.POST.get("status","")
+    #
+    #     if project_name == "":
+    #
+    #         return render(request,"project.html",{"type":"add","name_error":"项目名称不能为空"})
+    #
+    #     else:
+    #
+    #         Project.objects.create(name=project_name,describe=project_describe,status=status,
+    #                                create_time=datetime(2019,4,3,00,10,00))
+    #
+    #         return HttpResponseRedirect("/project/")
