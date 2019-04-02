0183be1e0d2aff3c9ffc50d1ffe7c4869944cf7e
zhangbo666 <78038490@qq.com>
2019-04-02 19:53:30 +0800

    2019-04-02 19:53 提交

--- test_platform/personal/views/project_views.py
+++ test_platform/personal/views/project_views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 from personal.models.project import Project
 
+import datetime
 
 # 登录成功，默认项目管理页
 @login_required
@@ -21,4 +22,25 @@ def project_manage(request):
 @login_required
 def add_project(request):
 
-    return render(request,"project.html",{"type":"add"})
+    if request.method == 'GET':
+
+        return render(request,"project.html",{"type":"add"})
+
+    elif request.method == 'POST':
+
+        project_name = request.POST.get("project_name","")
+
+        project_describe = request.POST.get("project_describe","")
+
+        status = request.POST.get("status","")
+
+        print (project_name)
+        print (project_describe)
+        print (status)
+
+        Project.objects.create(name=project_name,describe=project_describe,status=status,
+                               create_time=datetime(2019,04,02,19,50,00))
+
+
+        # return render(request,"project.html",{"type":"add"})
+
