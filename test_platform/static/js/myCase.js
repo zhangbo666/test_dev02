574d30cecc723c51c54db4618579335dfaa14163
zhangbo666 <78038490@qq.com>
2019-05-07 19:44:54 +0800

    2019-05-07 19:44

---
+++ test_platform/static/js/myCase.js
@@ -0,0 +1,19 @@
+/**
+ * Created by zhangbo on 2019/5/7.
+ */
+
+// 获取项目列表
+var projectInit = function(){
+
+    var options = "";
+
+    function getprojectListInfo(){
+
+
+        $.get("/project/get_project_list/",{},function () {
+            
+        })
+    }
+
+
+}
