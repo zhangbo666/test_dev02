f6eab639d7dff85cd9c17e1887a3b3c9d3fa3f9a
zhangbo666 <78038490@qq.com>
2019-05-09 08:10:51 +0800

    2019-05-09 08:10

--- test_platform/static/js/myCase.js
+++ test_platform/static/js/myCase.js
@@ -3,12 +3,31 @@
  */
 
 // 获取项目列表
-var projectInit = function(){
+var projectInit = function(_cmbProject){
+
+    var cmbProject = document.getElementById(_cmbProject);
 
     var options = "";
 
-    function getProjectListInfo(){
+    // 创建下拉选项
+    function cmbAddOption(cmb,str){
 
+        console.log(str);
+
+        var option = document.createElement("option");
+
+        cmb.options.add(option);
+
+        option.innerHTML = str;
+
+        option.value = str;
+
+        // option.obj = obj;
+
+
+    }
+
+    function getProjectListInfo(){
 
         $.get("/project/get_project_list/",{},function (resp) {
 
@@ -16,6 +35,13 @@ var projectInit = function(){
 
                 console.log(resp.data);
 
+                let dataList = resp.data;
+
+                for(var i = 0; i < dataList.length; i++){
+
+                    cmbAddOption(cmbProject,dataList[i],dataList[i]);
+
+                }
             }
         })
     }
