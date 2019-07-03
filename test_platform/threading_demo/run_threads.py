4c7f61aaaaf85555cba0c6dde14b3e27955a20c0
zhangbo666 <78038490@qq.com>
2019-07-03 08:05:08 +0800

    2019-07-03 08:05

--- test_platform/threading_demo/run_threads.py
+++ test_platform/threading_demo/run_threads.py
@@ -1,21 +1,46 @@
+#!/usr/bin/python3
+#encoding:utf-8
+
 __author__ = 'zhangbo'
 
 
 import threading
+import time
+
 
 def task1():
 
-    print ("任务1")
+    print ("task_1")
+    time.sleep(2)
 
 
 def task2():
 
-    print ("任务2")
+    print ("tasl_2")
+    time.sleep(3)
 
 
+def run_talk():
+
+    threads = []
+
+    t1 = threading.Thread(target=task1())
+
+    threads.append(t1)
+
+    t2 = threading.Thread(target=task2())
+
+    threads.append(t2)
+
+    for t in threads:
+
+        t.run()
+
+    for t in threads:
+
+        t.join()
 
 if __name__ == '__main__':
 
-
-    task1()
-    task2()
+    run_talk()
+    print ("finish end")
